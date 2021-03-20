0d7726be0967b342fa14aa697006218c2a1e7b81
Oliver Read <oliver@reads.org.uk>
2021-03-20 12:32:53 +0000

    Add sound effects

--- src/objects/card/card.gd
+++ src/objects/card/card.gd
@@ -3,11 +3,15 @@ extends TextureButton
 var type: String
 var face: Texture
 var back: Texture
+var audio: AudioStream;
 
 func init(new_type):
 	type = new_type
 	face = load("res://assets/textures/" + str(type) + ".png")
 	back = load("res://assets/textures/card_back.png")
+	audio = load("res://assets/audio/" + str(type) + ".wav")
+	if audio:
+		$AudioStreamPlayer.set("stream", audio)
 	set_normal_texture(back)
 
 func flip():
@@ -15,4 +19,5 @@ func flip():
 		set_normal_texture(back)
 	else:
 		set_normal_texture(face)
+		$AudioStreamPlayer.play()
 		
